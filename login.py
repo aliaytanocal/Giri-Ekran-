552e719238759df07a1037c8aa6b3aa9ef46bdc5
Ali Aytan Öçal <36601746+aliaytanocal@users.noreply.github.com>
2022-11-23 22:47:55 +0300

    Add files via upload

---
+++ login.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+from tkinter import *
+
+
+def submitact():
+     
+    user = Username.get()
+    passw = password.get()
+  
+    print(f"The name entered by you is {user} {passw}")
+  
+    logintodb(user, passw)
+
+
+
+root = tk.Tk()
+root.geometry("300x300")
+root.title("Giriş Ekranı")
+root.configure(bg="Green")
+
+
+lblfrstrow = tk.Label(root, text ="Kullanıcı Adı -", )
+lblfrstrow.place(x = 50, y = 20)
+ 
+kullanıcıadı = tk.Entry(root, width = 35)
+kullanıcıadı.place(x = 150, y = 20, width = 100)
+  
+lblsecrow = tk.Label(root, text ="Parola-")
+lblsecrow.place(x = 50, y = 50)
+ 
+parola = tk.Entry(root, width = 35)
+parola.place(x = 150, y = 50, width = 100)
+ 
+submitbtn = tk.Button(root, text ="Giriş",
+                      bg ='blue', command = submitact)
+submitbtn.place(x = 150, y = 135, width = 55)
+ 
+root.mainloop()
